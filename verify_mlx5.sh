b048b73184c9e2d4d1df043c9f3c32c9855a808b
Ren Zhenghang <renzhenghang@hotmail.com>
2023-06-06 17:19:59 +0800

    fix file name

--- verify_mlx5.sh
+++ verify_mlx5.sh
@@ -9,4 +9,4 @@ dpdk_dir=dpdk-stable-22.11.2
 test_dir=$dpdk_dir/build/app/test
 dev_addr=$1
 
-$test_dir/test_dpdk -c 1 -n 1 -a $dev_addr,class=crypto cryptodev_mlx5_autotest
+$test_dir/dpdk-test -c 1 -n 1 -a $dev_addr,class=crypto cryptodev_mlx5_autotest
