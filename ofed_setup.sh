9f969695953e4db327e6734a33923ed3d6745a97
Ren Zhenghang <renzhenghang@hotmail.com>
2023-06-06 15:32:06 +0800

    update ofed

--- ofed_setup.sh
+++ ofed_setup.sh
@@ -1,4 +1,3 @@
 wget -c https://content.mellanox.com/ofed/MLNX_OFED-5.8-2.0.3.0/MLNX_OFED_LINUX-5.8-2.0.3.0-ubuntu22.04-x86_64.tgz -O - | tar xz
 cd MLNX_OFED_LINUX-5.8-2.0.3.0-ubuntu22.04-x86_64
-sudo su
 ./mlnxofedinstall --all
